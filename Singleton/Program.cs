f564bfc9a00f373b872411342935d0218409a5a1
Guip94 <pinchartg94.proftl@gmail.com>
2025-01-26 19:58:27 +0100

    commentaires

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -16,3 +16,9 @@ Console.WriteLine(cache2.Get("test"));
 
 Console.WriteLine(object.ReferenceEquals(cache1, cache2));  // ==> même référence mémoire
 
+// A noter que l'implémentation du singleton comme ici viole l'un des principes SOLID
+// le S : single respinsibility car ici,
+//Memory cache a 2 responsabilités. La première est de gérer le cache
+// la deuxième est de gérer sa propre durée de vie, son cycle.
+
+// => pas recommandé  à la mano
